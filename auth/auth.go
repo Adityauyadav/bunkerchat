efaa9675a6a8afd3ac91c48b730e457281cf1a97
Aditya Yadav <adi.addie.yadav@gmail.com>
2026-03-11 05:25:07 +0530

    auth layer with bcrypt and JWT

--- auth/auth.go
+++ auth/auth.go
@@ -1,1 +1,59 @@
 package auth
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var secretKey = os.Getenv("JWT_SECRET_KEY")
+var JWTSecret = []byte(secretKey)
+
+func HashPassword(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hash), nil
+
+}
+
+func CheckPassword(password string, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+
+}
+
+func GenerateToken(userID int, username string) (string, error) {
+	claims := jwt.MapClaims{
+		"id":       userID,
+		"username": username,
+		"exp":      time.Now().Add(time.Hour * 24).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, err := token.SignedString(JWTSecret)
+	if err != nil {
+		return "", err
+	}
+	return signedToken, nil
+
+}
+
+func ValidateToken(tokenString string) (int, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &jwt.MapClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return JWTSecret, nil
+	})
+	if err != nil {
+		return 0, err
+	}
+	claims, ok := token.Claims.(*jwt.MapClaims)
+	if !ok {
+		return 0, fmt.Errorf("invalid token claims")
+	}
+	id := (*claims)["id"].(float64)
+	return int(id), nil
+
+}
--- server/main.go
+++ server/main.go
@@ -6,14 +6,22 @@ import (
 	"net/http"
 
 	"github.com/adityauyadav/bunkerchat/db"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	fmt.Println("BunkerChat server starting...")
 	db.Init()
+
 	http.HandleFunc("/health", healthHandler)
+
 	log.Println("Server listening on port 8080")
-	err := http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
